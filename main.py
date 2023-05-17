29ad152a18f67de0946f77e6abc671f101ef3640
rsurge <sgpatelre@gmail.com>
2023-05-17 12:53:14 -0700

    Third commit

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
     user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+    elif user_letter in used_letters:
+        print('You have already used that character. Please try again.')
+    else:
+        print('Invalid character. Please try again.')
 
 
 user_input = input('Type something: ')
